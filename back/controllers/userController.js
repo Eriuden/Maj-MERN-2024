7f9ad08f5a89f102cacd46b0c2b171dcefcc0a8d
Eriuden <antoinecarrette@hotmail.fr>
2024-01-03 09:31:32 +0100

    controllers routes 1

---
+++ back/controllers/userController.js
@@ -0,0 +1,4 @@
+module.exports.authUser = (req,res) => {
+    res.status(200).json({message:"auth"})
+}
+
--- back/index.js
+++ back/index.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const port = process.env.PORT
+import userRoutes from "./routes/userRoutes"
 
 const app = express()
+
+app.use("/api/users", userRoutes)
 
 app.get("/", (req,res) => res.send("Serveur prêt à l'action "))
 
