d74f8063beb35af14559454ac00410860a80a008
Eriuden <antoinecarrette@hotmail.fr>
2024-01-04 20:02:51 +0100

    userController 1

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -4,3 +4,19 @@ module.exports.authUser = asyncHandler(async(req,res) => {
     res.status(200).json({message:"auth"})
 })
 
+module.exports.registerUser = asyncHandler(async(req,res) => {
+    res.status(200).json({message:"inscrit"})
+})
+
+module.exports.logoutUser = asyncHandler(async(req,res) => {
+    res.status(200).json({message:"Déconnecté"})
+})
+
+module.exports.getUserProfile = asyncHandler(async(req,res) => {
+    res.status(200).json({message:"Profil utilisateur"})
+})
+
+module.exports.updateUserProfile = asyncHandler(async(req,res) => {
+    res.status(200).json({message:"Profil utilisateur mis à jour"})
+})
+
