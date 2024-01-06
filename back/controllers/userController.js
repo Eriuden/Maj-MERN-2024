d4bb7589a18b3fff738ef9d221357fcf7ddefe25
Eriuden <antoinecarrette@hotmail.fr>
2024-01-06 09:06:09 +0100

    userController 2

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,10 +1,36 @@
 import asyncHandler from "express-async-handler"
+import User from "../models/userModel"
 
 module.exports.authUser = asyncHandler(async(req,res) => {
     res.status(200).json({message:"auth"})
 })
 
 module.exports.registerUser = asyncHandler(async(req,res) => {
+    const {name, email, password} = req.body
+
+    const userExists = await User.findOne({email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error("Utilisateur déjà enregistré")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error("Données invalides")
+    }
     res.status(200).json({message:"inscrit"})
 })
 
