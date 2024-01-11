cddaafe92adc8e5b399ce844f72cd446141b53be
Eriuden <antoinecarrette@hotmail.fr>
2024-01-11 08:55:20 +0100

    front settings

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT,
+    proxy: {
+      "/api": {
+        target: process.env.API_PORT,
+        changeOrigin: true
+      }
+    }
+  }
 })
