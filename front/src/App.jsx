e45e0f118cc7ed5b4febbc77c642adafb79b9bb6
Eriuden <antoinecarrette@hotmail.fr>
2024-01-12 21:28:55 +0100

    homeScreen

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
+import { homeScreen } from './screen/homeScreen'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <homeScreen/>
     </>
   )
 }
---
+++ front/src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Hero = () => {
+  return (
+    <div>
+        <Link to={"/signin"}>Inscription</Link>
+    </div>
+  )
+}
