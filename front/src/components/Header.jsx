d2e4936881d7c5a6d13fa4c5e086a3f313e191b7
Eriuden <antoinecarrette@hotmail.fr>
2024-01-25 11:31:10 +0100

    header nav

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useSelector, useDispatch} from "react-redux"
 
 export const Header = () => {
+
+  const {userInfo} = useSelector((state)=> state.auth)
   return (
     <div>
         <h1>MERN app</h1>
         <nav>
+          {userInfo ? (
+            <>
+              <h3>Bienvenue {userInfo.name}</h3>
+              <Link to={"/profile"}>Profil</Link>
+              <h3>Déconnexion</h3>
+            </>) : (
             <Link to={"/login"}>Se connecter</Link>
+          )}
         </nav>
     </div>
   )
